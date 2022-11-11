e6f378b57d32dd70819b199c40bc123135662b14
daianhdung <daianhdung99@gmail.com>
2022-11-11 21:28:05 +0700

    add spring security

---
+++ src/main/java/com/example/food_project/security/SecSecurityConfig.java
@@ -0,0 +1,84 @@
+package com.example.food_project.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@EnableWebSecurity
+@Component
+public class SecSecurityConfig implements AuthenticationProvider {
+    //Dùng để khởi tạo danh sách user cứng và danh sách user này được lưu ở RAM
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService(){
+//        UserDetails user1 = User.withUsername("cybersoft")
+//                .password(passwordEncoder().encode("123")).roles("USER")
+//                .build();
+//
+//        UserDetails user2 = User.withUsername("admin")
+//                .password(passwordEncoder().encode("admin123")).roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(user1, user2);
+//    }
+
+    @Autowired
+    CustomAuthenticationProvider customAuthenticationProvider;
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider);
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //Qui định các rule liên quan đến bảo mật và quyeền truy cập
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        /*
+        * antMatcher : Định nghĩa link cần xác thực
+        * authenticated: Bắt buộc phải chứng thực (đăng nhập) vào link chỉ định ở antMatchers
+        */
+
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/signin").permitAll()
+                .antMatchers("/signin/test").authenticated()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
+
